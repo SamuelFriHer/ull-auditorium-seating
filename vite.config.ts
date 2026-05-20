85e96dc3ff46e964a3dc518ab236e17fdca42f8e
Samuel Frías <samuelfriher@gmail.com>
2026-05-20 13:26:22 +0100

    Update configuration files: refine package.json, tsconfig.json, and add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  base: '/ull-auditorium-seating/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+  test: {
+    globals: true,
+    environment: 'node',
+  },
+});
